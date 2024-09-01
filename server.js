77449c977fce192d712cebbb339b30975dc50eaf
Dhalintin <uchexdhalitin@gmail.com>
2024-09-01 22:05:52 +0100

    Feat: changed from Socket.io to SDK

--- server.js
+++ server.js
@@ -1,33 +1,75 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { default: fetch } = require("node-fetch");
+const jwt = require("jsonwebtoken");
+
+const PORT = 9000;
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const {v4: uuidv4} = require('uuid')
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-    res.redirect(`/${uuidv4()}`)
-})
-
-app.get('/:id', (req, res) => {
-    res.render('room', {roomId: req.params.id})
+//
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+//
+app.get("/get-token", (req, res) => {
+  const API_KEY = process.env.VIDEOSDK_API_KEY;
+  const SECRET_KEY = process.env.VIDEOSDK_SECRET_KEY;
 
+  const options = { expiresIn: "10m", algorithm: "HS256" };
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-connected', userId)
+  const payload = {
+    apikey: API_KEY,
+    permissions: ["allow_join", "allow_mod"], // also accepts "ask_join"
+  };
 
-
-        socket.on('disconnect', ()=> {
-            socket.broadcast.to(roomId).emit('user-disconnected', userId)
-        })
-    });    
+  const token = jwt.sign(payload, SECRET_KEY, options);
+  res.json({ token });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT);
+//
+app.post("/create-meeting/", (req, res) => {
+  const { token, region } = req.body;
+  const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings`;
+  const options = {
+    method: "POST",
+    headers: { Authorization: token, "Content-Type": "application/json" },
+    body: JSON.stringify({ region }),
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((result) => res.json(result)) // result will contain meetingId
+    .catch((error) => console.error("error", error));
+});
+
+//
+app.post("/validate-meeting/:meetingId", (req, res) => {
+  const token = req.body.token;
+  const meetingId = req.params.meetingId;
+
+  const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings/${meetingId}`;
+
+  const options = {
+    method: "POST",
+    headers: { Authorization: token },
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((result) => res.json(result)) // result will contain meetingId
+    .catch((error) => console.error("error", error));
+});
+
+//
+app.listen(PORT, () => {
+  console.log(`API server listening at http://localhost:${PORT}`);
+});
